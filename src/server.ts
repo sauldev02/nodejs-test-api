6a7a12b868e6e4295d11d8659529fe815528c395
Saul <sauldev0220@gmail.com>
2025-06-07 20:52:56 -0400

    Normalize aliases and ignore generated files

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
-import { PrismaClient } from './generated/prisma';
+import prisma from '@infra/database/prisma/client';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
-const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
